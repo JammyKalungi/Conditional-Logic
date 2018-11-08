8eca06884e9cf191976f7bd9f3a94ad40e71d396
JammyKalungi <44286612+JammyKalungi@users.noreply.github.com>
2018-11-08 11:43:39 +0300

    Update Conditional Logic.py

--- Conditional Logic.py
+++ Conditional Logic.py
@@ -1,7 +1,7 @@
 Yearofbirth = int(input("Enter your year of birth "))
 if Yearofbirth >= 2000:
 	print("You are a minor")
-if Yearofbirth >= 1986:
+if Yearofbirth >= 1982:
 	print("You are a youth")
 else:
 	print("You are an adult")
